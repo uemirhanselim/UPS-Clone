b767aed5d0c7409e4350763df554ceb384d92c66
uemirhanselim <uemirhanselim@gmail.com>
2023-03-03 21:03:02 +0300

    apollo client added

--- App.tsx
+++ App.tsx
@@ -3,15 +3,23 @@ import {TailwindProvider} from 'tailwind-rn';
 import utilities from './tailwind.json';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './navigator/RootNavigator';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://dashboard.stepzen.com/explorer?endpoint=api%2Fwayfaring-liger',
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
     // @ts-ignore - TailwindProvider is missing a type definition
     
     <TailwindProvider utilities={utilities} >
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
+      <ApolloProvider client={client}>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </ApolloProvider>
     </TailwindProvider>
   );
 }
